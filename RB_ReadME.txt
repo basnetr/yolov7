a99edccf60f75398e72482c927bed6eeabda2881
Ramesh Basnet <rameshbasnet@outlook.com>
2025-08-11 19:32:09 +0000

    update readme

--- RB_ReadME.txt
+++ RB_ReadME.txt
@@ -9,4 +9,15 @@ Environment Setup:
 conda create -n pose python=3.8
 conda activate pose
 python -m pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+Inside the cloned repo directory:
 python -m pip install -r requirements.txt
+
+Data prep:
+To prepare dataset for keypoints -- update get_coco.sh file based as you need
+bash /home/ubuntu/yolov7_pytorch_pose/yolov7/data/scripts/get_coco.sh
+
+Train:
+python train.py
+
+Test:
+python test.py
--- data/scripts/get_coco.sh
+++ data/scripts/get_coco.sh
@@ -8,11 +8,12 @@
 #     /yolov5
 
 # bash /home/ubuntu/yolov7_pytorch_pose/yolov7/data/scripts/get_coco.sh
-mkdir -p /opt/dlami/nvme/pose/
 mkdir -p /opt/dlami/nvme/pose/coco/images
 
+cd /opt/dlami/nvme/pose/
+
 # Download/unzip labels
-d='/opt/dlami/nvme/pose/' # unzip directory
+d='/opt/dlami/nvme/pose/coco' # unzip directory
 # url=https://github.com/ultralytics/yolov5/releases/download/v1.0/
 url=https://github.com/WongKinYiu/yolov7/releases/download/v0.1/
 f='coco2017labels-keypoints.zip' # 'coco2017labels.zip' # or 'coco2017labels-segments.zip', 68 MB
@@ -24,20 +25,18 @@ d='/opt/dlami/nvme/pose/coco/images' # unzip directory
 url=http://images.cocodataset.org/zips/
 f1='train2017.zip' # 19G, 118k images
 f2='val2017.zip'   # 1G, 5k images
-f3='test2017.zip'  # 7G, 41k images (optional)
+# f3='test2017.zip'  # 7G, 41k images (optional)
 for f in $f1 $f2; do
   echo 'Downloading' $url$f '...'
   curl -L $url$f -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
 done
 wait # finish background tasks
 
-# After downloading, move files:
-# rename original directories inside coco:
-# mv labels labels_detection
-# mv train2017.txt train2017.txt_detection
-# mv test-dev2017.txt test-dev2017.txt_detection
+# rename keypoints labels and files - remove suffix if you want to use full dataset
+mv /opt/dlami/nvme/pose/coco/train2017.txt /opt/dlami/nvme/pose/coco/train2017.txt_full_dataset_keypoints
+mv /opt/dlami/nvme/pose/coco/val2017.txt /opt/dlami/nvme/pose/coco/val2017.txt_full_dataset_keypoints
 
-# And move keypoint files to original locations:
-# mv labels coco/labels
-# mv train2017.txt coco/train2017.txt
-# mv val2017.txt coco/val2017.txt
+# comment this if you want to use full dataset
+# prepare small sample dataset
+cp /home/ubuntu/yolov7_pytorch_pose/yolov7/data/train2017.txt /opt/dlami/nvme/pose/coco/train2017.txt
+cp /home/ubuntu/yolov7_pytorch_pose/yolov7/data/test2017.txt /opt/dlami/nvme/pose/coco/val2017.txt
